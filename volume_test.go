6077297d4c8e715d082d40bbf175498ac2b8b4a6
Colin Benner <colin-github@yzhs.de>
2019-01-31 06:33:09 +0100

    Add more tests

--- volume_test.go
+++ volume_test.go
@@ -81,3 +81,61 @@ func TestParseArgumentsDecrement(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestParseArgumentsGuiMute(t *testing.T) {
+	arg, unmute, gui := parseArguments([]string{"-x", "mute"})
+
+	if arg != "mute" {
+		t.Fail()
+	}
+
+	if unmute {
+		t.Fail()
+	}
+
+	if !gui {
+		t.Fail()
+	}
+}
+
+func TestParseArgumentsUnmute(t *testing.T) {
+	arg, unmute, gui := parseArguments([]string{"unmute"})
+
+	if arg != "unmute" {
+		t.Fail()
+	}
+
+	if !unmute {
+		t.Fail()
+	}
+
+	if gui {
+		t.Fail()
+	}
+}
+
+func TestParseArgumentsToggle(t *testing.T) {
+	arg, unmute, gui := parseArguments([]string{"toggle"})
+
+	if arg != "toggle" {
+		t.Fail()
+	}
+
+	if !unmute {
+		t.Fail()
+	}
+
+	if gui {
+		t.Fail()
+	}
+}
+
+func TestParseArgumentsFailure(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		}
+	}()
+
+	parseArguments([]string{"foo"})
+}
